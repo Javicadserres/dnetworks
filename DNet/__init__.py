1f6c45570ee53c1fba14d544e735ccb6ac1a765a
Javicadserres <javier.cardenas.serres@gmail.com>
2021-02-06 16:40:16 +0100

    DOC: added versioneer.

--- DNet/__init__.py
+++ DNet/__init__.py
@@ -4,3 +4,6 @@ from . import layers
 
 from .optimizers import SGD, RMSprop, Adam
 from .model import NNet
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
