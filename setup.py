4587a462b4521aa2063988f6644cc1f29de57222
Javicadserres <javier.cardenas.serres@gmail.com>
2021-10-30 17:29:26 +0200

    ENH: add LSTMcell and fix setup

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from setuptools import setup, find_packages
 import versioneer
 
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='DNet',
+    name='dnet',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Deep Neural Network implementation in numpy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Javier Cárdenas',
+    package_dir={'':'src'},
+    packages=find_packages(where='src'),
     install_requires=[
         "numpy"
     ],
+    cmdclass=versioneer.get_cmdclass(),
 )
--- src/dnet/layers/recurrent.py
+++ src/dnet/layers/recurrent.py
@@ -1,7 +1,7 @@
 import numpy as np
 from .base import RNNBase
 from .linear import LinearLayer
-from .activation import Tanh, Softmax
+from .activation import Tanh, Softmax, Sigmoid
 
 
 class RNNCell(RNNBase):
@@ -206,3 +206,119 @@ class RNN(RNNBase):
         """
         self.lineal.optimize(method)
         self.rnn_cell.optimize(method)
+
+
+class LSTMCell(RNNBase):
+    """
+    Recurrent neural network cell implementation.
+    """
+    def __init__(self, input_dim, hidden_dim):
+        """
+        Initialize the parameters with the input and hidden
+        dimensions. 
+
+        Parameters
+        ----------
+        input_dim : int
+            Dimension of the input. 
+        hidden_dim : int
+            Number of units in the RNN cell.
+        """
+        super(LSTMCell, self).__init__(input_dim, hidden_dim)
+
+        self.lineal_f = LinearLayer(input_dim + hidden_dim, hidden_dim)
+        self.sigmoid_f = Sigmoid()
+
+        self.lineal_i = LinearLayer(input_dim + hidden_dim, hidden_dim)
+        self.sigmoid_i = Sigmoid()
+
+        self.lineal_tanh = LinearLayer(input_dim + hidden_dim, hidden_dim)
+        self.tanh_i = Tanh()
+
+        self.lineal_o = LinearLayer(input_dim + hidden_dim, hidden_dim)
+        self.sigmoid_o = Sigmoid()
+ 
+        self.tanh_c = Tanh()
+
+
+    def forward(self, input_X, hidden=None, cell_state=None):
+        """
+        Computes the forward propagation of the RNN.
+
+        Parameters
+        ----------
+        input_X : numpy.array or list
+            List containing all the inputs that will be used to 
+            propagete along the RNN cell.
+
+        Returns
+        -------
+        hidden : numpy.array
+            Array containing the output of the hidden state.
+        """
+        if hidden is None: hidden = np.zeros((self.hidden_dim, 1))
+
+        # combine the input with the hidden state
+        self.combined = np.concatenate((input_X, hidden), axis=0)
+
+        forget_input = self.lineal_f.forward(self.combined)
+        forget_layer = self.sigmoid_f.forward(forget_input)
+        
+        input_input = self.lineal_i.forward(self.combined)
+        input_layer = self.sigmoid_i.forward(input_input)
+
+        tanh_input = self.lineal_tanh.forward(self.combined)
+        tanh_layer = self.tanh_i.forward(tanh_input)
+
+        output_input = self.lineal_o.forward(self.combined)
+        output_layer = self.sigmoid_o.forward(output_input)
+
+        cell_state = forget_layer * cell_state + input_layer * tanh_layer
+
+        _tanh_c = self.tanh_c.forward(cell_state)
+
+        hidden = output_layer * _tanh_c
+
+        return hidden, cell_state
+
+
+    def backward(self, dZ, d_hidden=0, hidden=None, combined=None):
+        """
+        Computes the backward propagation of the model.
+
+        Parameters
+        ----------
+        dZ : numpy.array
+            The gradient of the of the output with respect to the
+            next layer.
+
+        Returns
+        -------
+        d_hidden : numpy.array
+            The gradient of the input with respect to the current 
+            layer.
+        """  
+        # derivative of the hyperbolic tangent
+        self.tanh.A = hidden
+        d_tanh_c = self.tanh_c.backward(dZ)
+
+        # derivative of the hidden state
+        parameters = self._retrieve_parameters(self.lineal, d_hidden)
+        self.lineal.A = combined
+        d_hidden = self.lineal.backward(d_tanh_c)
+        self.lineal = self._update_parameters(self.lineal, parameters)
+
+        return d_hidden[-self.hidden_dim:, :]
+
+
+    def optimize(self, method):
+        """
+        Updates the parameters of the model using a given optimize 
+        method.
+
+        Parameters
+        ----------
+        method: Class
+            Method to use in order to optimize the parameters.
+        """
+        self.lineal.optimize(method)
